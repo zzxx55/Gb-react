a616c11caf3f12acc8bc382e98a20c87091d6049
zzxx55 <87701940+zzxx55@users.noreply.github.com>
2022-11-30 14:59:08 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import "./App.css";
+import Message from "./components/Message";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Message text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat, eaque." />
+      </header>
+    </div>
+  );
+}
+
+export default App;
